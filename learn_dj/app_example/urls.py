3f35cd4ae55c8fbc6be944aa7fd1c70f430be3f2
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2024-03-23 20:10:00 +0000

    Dynamic URLs

--- learn_dj/app_example/urls.py
+++ learn_dj/app_example/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<topic>/', views.dynamic_view),
+    path('<int:num_1>/<int:num_2>', views.sum),
 ]
--- learn_dj/app_example/views.py
+++ learn_dj/app_example/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("The is a new view!")
+
+
+dynamic_pages = {
+    'a': 'This is a dynamic view "A"',
+    'b': 'This is a dynamic view "B"',
+}
+
+
+def dynamic_view(request, topic):
+    return HttpResponse(dynamic_pages[topic])
+
+
+def sum(request, num_1, num_2):
+    return HttpResponse(str(num_1 + num_2))
