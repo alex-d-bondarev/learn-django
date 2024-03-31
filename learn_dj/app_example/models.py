401a6fea8bb59d37d7c425a9f6dbbe301e8319e2
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2024-03-31 14:40:41 +0100

    Add INSERT method impl

--- learn_dj/app_example/models.py
+++ learn_dj/app_example/models.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import pytest
+
 from .my_sql_connection import connect_to_mysql
 
 CONFIG = {
@@ -21,13 +25,14 @@ class Transaction:
     def disconnect(self):
         if self.connection is not None:
             self.connection.close()
+            self.connection = None
 
     def get_all(self):
         rows = list()
-        self.connect()
         query = """ SELECT id, income, expenditure, description, date_time, company_id 
                       FROM transaction;"""
 
+        self.connect()
         with self.connection.cursor(dictionary=True) as cursor:
             cursor.execute(query)
             rows = cursor.fetchall()
@@ -35,6 +40,33 @@ class Transaction:
 
         return rows
 
-def test_this():
-    all_transactions = {'transactions': Transaction().get_all()}
-    print('Ho!')
+    def insert_new_transaction(self, transaction_data):
+        insert_transaction_template = """
+        INSERT INTO transaction (income, expenditure, description, date_time, company_id) 
+             VALUES (%(income)s, %(expenditure)s, "%(description)s", %(date_time)s, %(company_id)s)"""
+
+        self.connect()
+        with self.connection.cursor() as cursor:
+            cursor.execute(insert_transaction_template, transaction_data)
+            self.connection.commit()
+
+        self.disconnect()
+
+
+# @pytest.mark.skip(reason='For debugging purpose')
+def test_get_all():
+    new_record = {
+        'income': 0,
+        'expenditure': 1,
+        'description': 'test_get_all()',
+        'date_time': datetime.now(),
+        'company_id': 1,
+    }
+
+    transaction_obj = Transaction()
+    before = transaction_obj.get_all()
+    transaction_obj.insert_new_transaction(new_record)
+    after = transaction_obj.get_all()
+
+    print(f'BEFORE:\n"{before}"\nAFTER:\n{after}')
+    assert len(before) + 1 == len(after)
